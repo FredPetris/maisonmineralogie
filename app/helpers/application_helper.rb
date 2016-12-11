ba1ba688b0f66dd2f4d107f738bd82e5975ea16c
Frederique Petris <frederique.petris@gmail.com>
2016-12-11 23:37:59 +0100

    wip on footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def randomized_background
     themes = [{color: "background-yellow", picture: "backgrounds/wulfenite.png"},
-              {color: "background-turquoise", picture: "backgrounds/3-aigue-marine_6cmx5cm_PAKISTAN"},
+              {color: "background-turquoise", picture: "backgrounds/aigue-marine_1717_11cmx12cm_A10-A1.png"},
               {color: "background-green", picture: "backgrounds/pyromorphite_6,5cm_x_7,5cm_Ussel-FRANCE.png"}]
     themes[rand(themes.size)]
   end
