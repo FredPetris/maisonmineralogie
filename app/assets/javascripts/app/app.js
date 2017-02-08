2e6300414b9e61e122b4d46ea1a114683180a522
Frederique Petris <frederique.petris@gmail.com>
2017-02-08 10:12:07 +0100

    WIP on animated modal

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,22 +1,4 @@
 $(document).ready(function(){
 
-  $("#modal-video-acteurs").animatedModal({
-    animatedIn:'zoomIn',
-    animatedOut:'zoomOut',
-    color:'#000000'
-  });
-
-  $("#modal-video-project").animatedModal({
-    animatedIn:'zoomIn',
-    animatedOut:'zoomOut',
-    color:'#000000'
-  });
-
-  var url = $(".video-container").find("iframe").attr('src');
-
-  $('.close-animatedModal').click(function() {
-    $(".video-container").fadeOut().find("iframe").attr("src", "");
-    $(".video-container").fadeIn().find("iframe").attr('src', url);
-  });
 
 });
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,5 @@
 //= require bootstrap-sprockets
 //= require underscore
 //= require jquery.slick
-//= require app/slick.js
 
 //= require_tree ./app
