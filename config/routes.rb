0312813d8f09cef2eec4500149196a5bc5bac22c
Frederique Petris <frederique.petris@gmail.com>
2017-03-17 11:01:31 +0100

    WIP video + logo + mentions légales

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
-  get 'contact',       to: 'pages#contact'
-  put 'newsletter',    to: 'pages#newsletter'
+  get 'contact',          to: 'pages#contact'
+  get 'mentions_legales', to: 'pages#mentions_legales'
+  put 'newsletter',       to: 'pages#newsletter'
 
 end
